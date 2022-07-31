4d3e1d1112c7e2be00c443645978532417a556a3
stomaras <sptoma724@gmail.com>
2022-07-31 20:00:58 +0300

    book

---
+++ Bookingg/Bookingg/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,140 @@
+﻿using Booking.Models;
+using Booking.Models.ViewModels;
+using Bookingg.Data;
+using Bookingg.DataAccess.Repository.Core;
+using Bookingg.DataAccess.Repository.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bookingg.Areas.Admin.Controllers
+{
+    /*
+     * Category Controller , we can take that to move into admin area, bcause that is a management controller
+     * which will be managed the category and only admins should be able to do that
+     * 
+     */
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        private ApplicationDbContext _db;
+        private UnitOfWork _unitOfWork;
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
+        {
+            _db = db;
+            _unitOfWork = new UnitOfWork(_db);
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+   
+
+        // GET
+        public IActionResult Upsert(int? id)
+        {
+            ProductViewModel productVM = new ProductViewModel()
+            {
+                product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i=> new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+
+            };
+            if (id is null || id == 0)
+            {
+                // Create Product
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                // tightly binded View
+                return View(productVM);
+
+            }
+            else
+            {
+                // update product
+            }
+           
+            return View(productVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductViewModel obj, IFormFile? file)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    // means file was uploaded
+                    
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRootPath, @"images\products");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName+extension),FileMode.Create))
+                    {
+                        file.CopyTo(fileStreams);
+                    }
+                    obj.product.ImageUrl = @"\images\products\" + fileName + extension;
+                }
+                _unitOfWork.Product.Add(obj.product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+            var CoverTypeFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (CoverTypeFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CoverTypeFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType Deleted Succesfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
